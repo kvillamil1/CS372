d0b62ae4e5312c22a56b3787075687ef334b8188
kvillamil1 <kvillamil16@my.whitworth.edu>
2015-01-20 16:13:20 -0800

    HW4(PR4_2: Android Coin Flip) Complete

---
+++ HW4(PR4_2)/app/src/main/java/com/example/kat/hw4pr4_2/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.kat.hw4pr4_2;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v) {
+        //does something when button is clicked
+       Button button = (Button) v;
+       TextView textOne = (TextView) findViewById(R.id.textCoin);
+       Random rnd = new Random();
+       int randomNumber = rnd.nextInt(2);
+
+       if(randomNumber == 0)
+        textOne.setText("Heads");
+       else
+        textOne.setText("Tails");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
